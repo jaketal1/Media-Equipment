0f1c83f2a7e244828b437875f748000e0ef95e0a
jaketal1 <40108017@hughbaird.ac.uk>
2025-02-25 11:55:22 +0000

    Update Backend.py

--- David/Media equipment/Backend.py
+++ David/Media equipment/Backend.py
@@ -4,17 +4,30 @@ first_name = input("Please Enter Your First Name ")
 
 last_name = input("Please Enter Your Last Name ")
 
-def get_student_id():
-    while True:
-        student_id = input("Please enter your student ID: ")
-        if is_valid(student_id):
-            return student_id
-        else:
-            print("Invalid student ID. Please try again.")
 
-def is_valid(student_id):
-    # Example validation: student_id should be numeric and 8 digits long
-    return student_id.isdigit() and len(student_id) == 8
+def checkid():
+    flag = False
+    max = 3
+    minlength = 8
+    attempts = 0
 
-student_id = get_student_id()
-print(f"Welcome, student {student_id}!")
+    while attempts < max:
+        login = input("Please enter your student ID")
+        if len(login) >= minlength: 
+             flag = True
+             return (login)
+        break
+    else:
+
+     attempts = attempts + 1
+     print("Student ID too short")
+     print("You have",{attempts}, "Remaining")
+
+     if attempts >= 3:
+        print("you have exceeded the maximum number of attempts")
+        print("All attempts used access denied")
+
+
+login = checkid()
+print(f"Welcome, student {login}!")
+
